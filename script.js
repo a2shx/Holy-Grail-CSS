1ac53a31aed4f784b5a0b0d2b2d4b6b97545a079
Chonrawee Charoenritichai <atx211me@gmail.com>
2025-01-24 12:13:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function expandNav() {
+  const outerGrid = document.querySelector('.outer-grid');
+  outerGrid.classList.toggle('outer-grid-hide');
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('hide-nav');
+}
